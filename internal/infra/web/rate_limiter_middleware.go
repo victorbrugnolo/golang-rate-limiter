0e698bbe66059a5f19a21e396e6acde17e595a25
victorbrugnolo <brugnolovictor@gmail.com>
2024-05-25 19:29:30 -0300

    add support to limit by token

--- internal/infra/web/rate_limiter_middleware.go
+++ internal/infra/web/rate_limiter_middleware.go
@@ -3,6 +3,7 @@ package web
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net"
 	"net/http"
 	"os"
@@ -14,23 +15,58 @@ import (
 
 func RateLimiterMiddleware(ctx context.Context, repository entity.RateLimiterDataRepositoryInterface, next func(w http.ResponseWriter, r *http.Request)) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		ip, _, err := net.SplitHostPort(r.RemoteAddr)
+		limit := 0
+		window := 0
+		blockWindow := 0
+		clientId := ""
 
-		if err != nil {
-			http.Error(w, "Error on getting client IP", http.StatusInternalServerError)
-			return
+		token := r.Header.Get("token")
+
+		if token != "" {
+			clientId = token
+
+			envPrefix := "RATE_LIMITER_BY_TOKEN_" + token
+			limit, _ = strconv.Atoi(os.Getenv(envPrefix + "_LIMIT"))
+			window, _ = strconv.Atoi(os.Getenv(envPrefix + "_WINDOW"))
+			blockWindow, _ = strconv.Atoi(os.Getenv(envPrefix + "_BLOCK_WINDOW"))
+
+			if limit == 0 || window == 0 || blockWindow == 0 {
+				w.Header().Set("Content-Type", "application/json")
+				w.WriteHeader(http.StatusOK)
+
+				message := entity.ApiResponse{
+					Message: fmt.Sprintf("Rate limiter configuration for token %s not found", token),
+				}
+
+				err := json.NewEncoder(w).Encode(message)
+
+				if err != nil {
+					http.Error(w, "Error on enconding response", http.StatusUnprocessableEntity)
+				}
+
+				return
+			}
+		} else {
+			ip, _, err := net.SplitHostPort(r.RemoteAddr)
+
+			if err != nil {
+				http.Error(w, "Error on getting client IP", http.StatusInternalServerError)
+				return
+			}
+
+			clientId = ip
+
+			limit, _ = strconv.Atoi(os.Getenv("RATE_LIMITER_BY_IP_LIMIT"))
+			window, _ = strconv.Atoi(os.Getenv("RATE_LIMITER_BY_IP_WINDOW"))
+			blockWindow, _ = strconv.Atoi(os.Getenv("RATE_LIMITER_BY_IP_BLOCK_WINDOW"))
 		}
-
-		limit, _ := strconv.Atoi(os.Getenv("RATE_LIMITER_BY_IP_LIMIT"))
-		window, _ := strconv.Atoi(os.Getenv("RATE_LIMITER_BY_IP_WINDOW"))
-		blockWindow, _ := strconv.Atoi(os.Getenv("RATE_LIMITER_BY_IP_BLOCK_WINDOW"))
 
 		windowDuration := time.Duration(window) * time.Second
 		blockWindowDuration := time.Duration(blockWindow) * time.Second
 
 		rateLimiterHandler := NewRateLimiter(limit, windowDuration, blockWindowDuration, repository)
 
-		if ok := rateLimiterHandler.HandleRateLimit(ctx, ip); !ok {
+		if ok := rateLimiterHandler.HandleRateLimit(ctx, clientId); !ok {
 			w.Header().Set("Content-Type", "application/json")
 			w.WriteHeader(http.StatusOK)
 
@@ -38,7 +74,7 @@ func RateLimiterMiddleware(ctx context.Context, repository entity.RateLimiterDat
 				Message: "you have reached the maximum number of requests or actions allowed within a certain time frame",
 			}
 
-			err = json.NewEncoder(w).Encode(message)
+			err := json.NewEncoder(w).Encode(message)
 
 			if err != nil {
 				http.Error(w, "Error on enconding response", http.StatusUnprocessableEntity)
